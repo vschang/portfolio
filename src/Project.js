7b627d3765e4baa569ab6ad29b0c25d3aea98b0b
Valerie Chang <valeriechang0@gmail.com>
2023-01-10 19:28:23 +0100

    removed use effectD

--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,5 @@
 import Modal from './Modal'
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import { Icon } from '@iconify/react';
 
 // Project is child of Project Section
